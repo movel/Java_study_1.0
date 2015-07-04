7a57e872bf912785462d6cc630a1645453bab0c2
movel <movel22@gmail.com>
2015-07-04 21:13:28 +0200

    Issue2

---
+++ Bug_3_ThemeOneIssueTwo/src/Theme1/ThemeOneIssueTwo.java
@@ -0,0 +1,38 @@
+package Theme1;
+public class ThemeOneIssueTwo {
+
+	public static void main (String[] args)
+	{
+		printMinMax("int");
+		printMinMax("byte");
+		printMinMax("char");
+	}	
+
+	public static void printMinMax(String type) {
+		long minValue = 0;
+		long maxValue = 0;
+		long startValue;
+		long endValue;
+		
+		if (type.equals("int")) {
+			startValue = Integer.MIN_VALUE;
+			endValue = Integer.MAX_VALUE;
+		} else if (type.equals("byte")) {			
+			startValue = Byte.MIN_VALUE;
+			endValue = Byte.MAX_VALUE;
+		} else if (type.equals("char")) {			
+			startValue = Character.MIN_VALUE;
+			endValue = Character.MAX_VALUE;
+		} else {
+			System.out.println("Invalid [type] argument");
+			return;
+		}
+		
+		for (long i = startValue; i <= endValue; i++) {
+			if (i < minValue) minValue = i;
+			if (i > maxValue) maxValue = i;			
+		}
+		
+		System.out.println(type + ". Min: " + minValue + ", Max: " + maxValue);
+	}	
+}
